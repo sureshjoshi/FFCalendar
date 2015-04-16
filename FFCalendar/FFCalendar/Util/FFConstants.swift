4ab19445f92632e84146f6454a274820265b2038
Fernanda <fggeraissate@gmail.com>
2015-04-16 17:32:01 -0300

    FFConstants: updated

--- FFCalendar/FFCalendar/Util/FFConstants.swift
+++ FFCalendar/FFCalendar/Util/FFConstants.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 enum ScrollDirection {
+    
     case None
     case Right
     case Left
@@ -21,13 +22,9 @@ let k_ARRAY_WEEK_ABREVIATION = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 let k_MINUTES_INTERVAL: Int = 4
 let k_MINUTES_PER_LABEL: Int = 60/k_MINUTES_INTERVAL
-let k_HEIGHT_CELL_HOUR: Int = 100
-let k_HEIGHT_CELL_MIN: Int = k_HEIGHT_CELL_HOUR/k_MINUTES_INTERVAL
-
-let k_DATE_MANAGER_DATE_CHANGED = "br.com.FFCalendar.DateManager.DateChanged"
-
-let k_HEADER_HEIGHT_SCROLL = 100
 
 let k_CELL = "cell"
 
 let isIphone = (UIDevice.currentDevice().userInterfaceIdiom == .Phone)
+
+let k_DATE_MANAGER_DATE_CHANGED = "br.com.FFCalendar.DateManager.DateChanged"
